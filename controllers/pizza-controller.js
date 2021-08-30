901a043252e0965a517a4933aa37b5287871addc
Bob Deckard <bdeckard92@gmail.com>
2021-08-30 09:12:52 -0500

    sets up pizza controller

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,37 @@
+const {Pizza} = require('./models');
+
+const pizzaController = {
+    //get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err=> {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+    //get one pizza by id
+  getPizzaById({params}, res) {
+      Pizza.findOne({_id: params.id})
+      .then(dbPizzaData => {
+          if (!dbPizzaData) {
+              res.status(404).json({message: 'no pizza for you'});
+              return;
+          }
+          res.json(dbPizzaData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+      })
+  },
+  //create a pizza
+  createPizza({body}, res) {
+      Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err))
+  }
+    
+};
+
+module.exports - pizzaController;
